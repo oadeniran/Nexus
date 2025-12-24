70e9eda80967dd5389c10dbf1106f25acd00066a
oadeniran <oadeniran82@gmail.com>
2025-12-24 09:20:30 +0100

    ensure creds is downloaded and only module deploy backend-deploy

--- backend/init_env.py
+++ backend/init_env.py
@@ -1,7 +1,19 @@
 from dotenv import load_dotenv
 import os
+import requests
+import json
 
 load_dotenv()
 
 # MongoDB connection setup
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+JSON_URL = os.getenv("JSON_URL")
+GOOGLE_APP_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS", "creds.json")
+
+# Download Google vertex Json
+response = requests.get(JSON_URL)
+if response.status_code == 200:
+    os.makedirs(os.path.dirname(GOOGLE_APP_CREDENTIALS), exist_ok=True)
+    with open(GOOGLE_APP_CREDENTIALS, "w") as file:
+        json_f = response.json()
+        json.dump(json_f, file)
